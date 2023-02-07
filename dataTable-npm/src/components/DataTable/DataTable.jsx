c3837af22989875ca7e2006d3c29e6dcba1d1cdc
Younes Lahouiti <loller763@gmail.com>
2023-02-07 10:59:45 +0100

    AJout de SASS pour le style

--- dataTable-npm/src/components/DataTable/DataTable.jsx
+++ dataTable-npm/src/components/DataTable/DataTable.jsx
@@ -3,13 +3,35 @@ import React from "react";
 /**
  * Data table with all the features from the jQuery counter part
  *
+ * @param {boolean} pagination Boolean to know whether to enable/disable pagination and disable/enable scrolling
+ *
+ * @param {string} title Title for the caption of the table
+ *
  * @param {{...property: value}[]} data Array of objects containig all the properties for the `<thead>` along with their values for the `<tbody>`
+ *
+ * ex:
+ * ```js
+ * data = [
+ * {
+ *  firstName: "Younes",
+ *  lastName: "Lahouiti"
+ * },
+ * {
+ *  firstName: "Walter",
+ *  lastName: "White"
+ * }
+ *  ]
+ *  //Will have the properties "Firstname" and "Lastname" in the <thead>
+ * //Will show the 2 rows with "Younes Lahouiti" and "Walter White" in the <tbody>
+ * ```
  *
  * @returns The table as a JSX element
  */
-export default function DataTable({ data }) {
+
+export default function DataTable({ title, data, pagination }) {
   return (
     <tbody>
+      <caption>{title}</caption>
       <thead></thead>
       <tfoot></tfoot>
       <tbody></tbody>
