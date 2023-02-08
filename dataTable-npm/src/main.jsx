a7b6f8701beb99e2e3ad10cc01092315408c8f9f
Younes Lahouiti <loller763@gmail.com>
2023-02-08 18:05:03 +0100

    Ajout de commentaires importnants + changement du main du package.json pour la bibliothèque

--- dataTable-npm/src/main.jsx
+++ dataTable-npm/src/main.jsx
@@ -10,6 +10,17 @@ import App from "./App";
 //Compiled CSS
 import "./style/css/style.css";
 
+/**
+ * Things to delete before uploading the library:
+ *
+ * -Uninstall all react-scripts
+ * ex: npm uninstall react-scripts --save
+ *
+ * - ReactDOM from this file since we're NOT creating an app and we want to export only the <DataTable/> component
+ * ex: export * from "./App.jsx" + rename "main.jsx" → "index.jsx"
+ *
+ * - The /public folder since we don't need logos
+ */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
