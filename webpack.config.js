e2ef8a0c4daf70211342cae04f042a912b48d9bb
Younes Lahouiti <loller763@gmail.com>
2023-02-19 14:23:09 +0100

    Ajout du bundler de Webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "commonjs react",
+  },
+};
